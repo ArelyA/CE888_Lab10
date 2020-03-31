32ce65442f0d9974d277c86568643ec92d89004d
Arely Aceves <arelyac01@gmail.com>
2020-03-31 20:56:46 +0100

    .

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import os
 import sys
 import pickle
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
