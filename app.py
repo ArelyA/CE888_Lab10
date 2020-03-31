7b5f8f28d04f8feb43224f8290eaf7982097ff5c
Arely Aceves <arelyac01@gmail.com>
2020-03-31 10:55:43 +0100

    Adapted to Heart Disease Dataset

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ with open('Models/RF_model.pkl', 'rb') as f:
 with open('Models/svm_clf_model.pkl', 'rb') as f:
     svm_model = pickle.load(f)
 
-
-def get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model):
-    mylist = [Driver_Age, Tax, price, Licence_Length_Years]
+# [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+def get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, req_model):
+    mylist = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
     mylist = [float(i) for i in mylist]
     vals = [mylist]
 
@@ -43,20 +43,29 @@ def my_form():
 
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
-    price = request.form['price']
-    Tax = request.form['Tax']
-    Driver_Age = request.form['Driver_Age']
-    Licence_Length_Years = request.form['Licence_Length_Years']
+    age = request.form['age']
+    sex = request.form['sex']
+    cp = request.form['cp']
+    trestbps = request.form['trestbps']
+    chol = request.form['chol']
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = request.form['thalach']
+    exang = request.form['exang']
+    oldpeak = request.form['oldpeak']
+    slope = request.form['slope']
+    ca = request.form['ca']
+    thal = request.form['thal']
     req_model = request.form['req_model']
 
-    target = get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model)
+    target = get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, req_model)
 
     if target==1:
-        sale_making = 'Customer is likely to buy the insurance'
+        pred_disease = 'Patien is likely to have heart disese'
     else:
-        sale_making = 'Customer is unlikely to buy the insurance'
+        pred_disease = 'Patien is unlikely to have heart disese'
 
-    return render_template('home.html', target = target, sale_making = sale_making)
+    return render_template('home.html', target = target, pred_disease = pred_disease)
 
 
 if __name__ == "__main__":
